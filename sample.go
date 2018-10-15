440e321e5832e12adb7637e462d0f154b4ad9de7
hirokisan <expsharetoworld@gmail.com>
2018-10-15 12:10:25 +0000

    Use naked return statement

--- sample.go
+++ sample.go
@@ -3,13 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	//fmt.Println(add(1, 3))
-	a, b := swap("hello", "world")
-	fmt.Println(a, b)
+	fmt.Println(add(1, 3))
+	//a, b := swap("hello", "world")
+	//fmt.Println(a, b)
 }
 
-func add(x, y int) int {
-	return x + y
+func add(x, y int) (result int) {
+	result = x + y
+	return
 }
 
 func swap(x, y string) (string, string) {
